3854aa0e3dc87dc394bb3b0f608fbb456c5e9e93
Roberto <Robertoaraujo230@hotmail.com>
2023-02-27 18:53:20 -0300

    Criando lógica de pegar os pokemons e construir seus cards

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+function getPokeLink(pokeID) {
+  return `https://pokeapi.co/api/v2/pokemon/${pokeID}`;
+}
+
+function getPokePromises() {
+  return Array(9)
+    .fill()
+    .map((promise, index) =>
+      fetch(getPokeLink(index + 1)).then((pokeData) => pokeData.json())
+    );
+}
+
+function createPokeObject(pokemon) {
+  return {
+    name: pokemon.name,
+    id: pokemon.id,
+    pixelSprite: pokemon.sprites.front_default,
+    officialSprite: pokemon.sprites.other["official-artwork"].front_default,
+    types: pokemon.types.map((item) => item.type.name),
+  };
+}
+
+function createPokeCardTypesHTML(pokemonType) {
+  if (pokemonType)
+    return `<span class="main__types--poke">${pokemonType}</span>`;
+  return "";
+}
+
+function createPokeCardHTML(pokemon) {
+  return `
+    <span class="main__id--poke">#${pokemon.id}</span>
+    <div class="main--container--pokeinfos">
+      <div class="main--container--pokecontent">
+        <h2 class="main__name--poke">${pokemon.name}</h2>
+        ${createPokeCardTypesHTML(pokemon.types[0])}
+        ${createPokeCardTypesHTML(pokemon.types[1])}
+      </div>
+      <div class="main--container--pokeimage">
+        <img src="${pokemon.officialSprite}" alt="" class="main__image--poke">
+      </div>
+    </div>
+  `;
+}
+
+function createPokesCard(pokemonData, containerClass) {
+  const fatherContainer = document.querySelector(containerClass);
+
+  pokemonData.forEach((pokemon) => {
+    const pokeCard = document.createElement("div");
+    const poke = createPokeObject(pokemon);
+
+    pokeCard.classList.add("main--container--poke", poke.types[0]);
+    pokeCard.innerHTML = createPokeCardHTML(poke);
+
+    fatherContainer.appendChild(pokeCard);
+  });
+}
+
+async function getPokeData() {
+  const allPokesPromises = await getPokePromises();
+  const allPokesData = await Promise.all(allPokesPromises);
+
+  createPokesCard(allPokesData, ".main--container--cards");
+}
+
+getPokeData();
