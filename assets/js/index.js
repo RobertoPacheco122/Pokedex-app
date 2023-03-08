b6811bfc3d040b854a18b3868a73a1bbb9cc83bf
Roberto <Robertoaraujo230@hotmail.com>
2023-03-07 22:04:32 -0300

    Criando funcionalidades para buscar o pokemon pelo nome

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,39 @@
 import Pokemon from "./modules/Pokemon.js";
 
-Pokemon.getData(9);
+const form = document.querySelector(".main__form");
+const searchInput = document.querySelector("#pokename");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+function removeAllPokeCards(pokeCardsClass) {
+  const cardsList = document.querySelectorAll(pokeCardsClass);
+  cardsList.forEach((card) => {
+    card.remove();
+  });
+}
+
+function handleChange(event) {
+  event.preventDefault();
+  removeAllPokeCards(".main--container--poke");
+
+  if (searchInput.value === "") {
+    Pokemon.getData(30);
+    return;
+  }
+
+  const inputValue = searchInput.value.toLowerCase();
+
+  try {
+    Pokemon.getPromise(inputValue).then((pokeObject) =>
+      Pokemon.createCard(pokeObject, ".main--container--cards")
+    );
+  } catch {
+    console.log("deu ruim meu chapão");
+  }
+}
+
+searchInput.addEventListener("change", handleChange);
+
+Pokemon.getData(30);
