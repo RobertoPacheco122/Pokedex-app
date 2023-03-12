dfe0a5381916f3a200e9658d5206b761d3d378a3
Roberto <Robertoaraujo230@hotmail.com>
2023-03-11 22:09:26 -0300

    Criando arquivo para a função renderCardTypesSection

---
+++ assets/js/render/renderCardTypes.js
@@ -0,0 +1,10 @@
+export default function renderCardTypes(pokemonType) {
+  if (pokemonType)
+    return `
+      <span class="main__types--poke">
+      <i class="main__icon--type icon icon--${pokemonType}"></i>
+      ${pokemonType}
+      </span>
+    `;
+  return "";
+}
