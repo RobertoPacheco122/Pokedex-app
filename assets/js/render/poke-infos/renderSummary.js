21967acb92c1a0a32ea8bdb734b2d02e8a1b302a
Roberto <Robertoaraujo230@hotmail.com>
2023-03-11 22:07:27 -0300

    Criando arquivo para a função renderSummarySection

---
+++ assets/js/render/poke-infos/renderSummary.js
@@ -0,0 +1,20 @@
+import renderCardTypes from "../renderCardTypes.js";
+
+export default function renderSummarySection(pokemon) {
+  return `
+    <div class="poke--container--name">
+      <h1 class="poke__name bold">${pokemon.name}</h1>
+      <span class="poke__id bold">#${pokemon.id}</span>
+    </div>
+    <div class="poke--container--type">
+      <ul class="poke__list--type">
+        ${renderCardTypes(pokemon.types[0])}
+        ${renderCardTypes(pokemon.types[1])}
+      </ul>
+    </div>
+    <div class="poke--container--image">
+      <img src="${pokemon.pixelSprite}" alt="" class="poke__image">
+      <img src="assets/img/pokeball.png" alt="" class="poke__image--ball">
+    </div>
+  `;
+}
