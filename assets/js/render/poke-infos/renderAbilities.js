02f939657305945e14fad54546480d968ba9eed3
Roberto <Robertoaraujo230@hotmail.com>
2023-03-11 22:08:06 -0300

    Criando arquivo para a função renderAbilitiesSection

---
+++ assets/js/render/poke-infos/renderAbilities.js
@@ -0,0 +1,9 @@
+export default function rendedrMovesSection(pokemon) {
+  const section = document.createElement("section");
+  section.classList.add("poke__section", "poke__section--moves");
+  section.innerHTML = `
+    
+  `;
+
+  return section;
+}
