aba45e91ec7b45e54af7d92d3d867b91ebb7fffa
Roberto <Robertoaraujo230@hotmail.com>
2023-03-11 22:04:55 -0300

    Criando arquivo para a função renderBaseStatsSection

---
+++ assets/js/render/poke-infos/renderBaseStats.js
@@ -0,0 +1,26 @@
+function createStatsContent(statsArray) {
+  const statusList = document.createElement("ul");
+  statusList.classList.add("poke__list--stats");
+
+  statsArray.forEach((stats) => {
+    const statusItem = document.createElement("li");
+    statusItem.classList.add("poke__item--stats");
+
+    statusItem.innerHTML = `
+      <span class="poke__stats--name grey--text">${stats[0].name}:</span> 
+      <span class="poke__stats--value">${stats[0].stat}</span> 
+    `;
+    statusList.appendChild(statusItem);
+  });
+
+  return statusList;
+}
+
+export default function renderBaseStatsSection(pokemon) {
+  const section = document.createElement("section");
+  section.classList.add("poke__section", "poke__section--stats");
+  section.innerHTML = `<h2 class="poke__subtitle bold">Stats</h2>`;
+  section.appendChild(createStatsContent(pokemon.stats));
+
+  return section;
+}
