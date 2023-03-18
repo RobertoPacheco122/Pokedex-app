914dcc144e28fd563131232967135958bf69938e
Roberto <Robertoaraujo230@hotmail.com>
2023-03-18 16:49:07 -0300

    Criando lógica para renderizar as habilidades dos pokemons

--- assets/js/render/poke-infos/renderAbilities.js
+++ assets/js/render/poke-infos/renderAbilities.js
@@ -1,8 +1,9 @@
-export default function rendedrMovesSection(pokemon) {
+export default function renderAbilitiesSection(pokemon) {
   const section = document.createElement("section");
   section.classList.add("poke__section", "poke__section--moves");
   section.innerHTML = `
-    
+    <h2 class="poke__subtitle bold">Abilities</h2>
+    <p class="poke__text capitalize">${pokemon.abilities}</p>
   `;
 
   return section;
