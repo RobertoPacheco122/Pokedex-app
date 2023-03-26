559d1a067600aaa2e483bc866b314e0e990d060e
Roberto <Robertoaraujo230@hotmail.com>
2023-03-25 23:42:13 -0300

    Separando a funcao createPokeInfos em um único arquivo

---
+++ assets/js/render/poke-infos/createPokeInfos.js
@@ -0,0 +1,25 @@
+import renderPokeInfos from "./renderPokeInfos.js";
+import getPokePromise from "../../utils/getPokePromise.js";
+import createPokeObject from "../../process/createPokeObject.js";
+import createPokeSpeciesObject from "../../process/createPokeSpeciesObject.js";
+import initTabNav from "../../utils/tabNav.js";
+
+async function createInfosHTML(pokemonID) {
+  const pokemonData = await getPokePromise("pokemon", pokemonID);
+  const pokemonSpeciesData = await getPokePromise("pokemon-species", pokemonID);
+  const pokemon = createPokeObject(pokemonData);
+  const pokemonSpecies = createPokeSpeciesObject(pokemonSpeciesData);
+
+  const body = document.querySelector("body");
+  body.classList.add(pokemon.types[0]);
+
+  await renderPokeInfos(
+    ".poke--container--infos",
+    ".poke--container--sections",
+    pokemon,
+    pokemonSpecies
+  );
+  initTabNav(".poke__link", ".poke__section");
+}
+
+export default createInfosHTML;
